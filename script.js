88ff3264d7367ed67ed27e136c7efc67d35f2937
karollima <anne.karol.lgn@gmail.com>
2021-02-12 16:00:41 -0300

    jogo da cobrinha quae pronto

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ snake [0] = {
     x: 8 * box,
     y: 8 * box
 }
-let direct = "right";
+let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG(){
     context.fillStyle = "lightgreen";
@@ -20,13 +24,19 @@ for(i=0; i < snake.length; i++){
     }
 }
 
+function drawFood (){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
+
 document.addEventListener('keydown', update);
 
 function update (event){
-    if(event.keyCode == 40 && direction != "right") direction = "left";
-    if(event.keyCode == 39 && direction != "down") direction = "up";
-    if(event.keyCode == 38 && direction != "left") direction = "right";
-    if(event.keyCode == 37 && direction != "up") direction = "down";
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
 }
 
 function iniciarJogo(){
@@ -37,6 +47,7 @@ function iniciarJogo(){
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY =snake[0].y;
@@ -46,7 +57,12 @@ function iniciarJogo(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else{ food.x = Math.floor(Math.random() * 15 + 1) * box;
+          food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,
